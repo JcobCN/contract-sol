3e3f7dd509bc7bc02e68dfc056fcb7d83fe9665a
t <t@t.com>
2023-03-28 16:13:21 +0800

    add arb network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,9 +13,22 @@ const config: HardhatUserConfig = {
   },
   networks:{
     bsc_testnet: {
-      url: `https://rpc.ankr.com/bsc_testnet_chapel/${process.env.API_KEY}`,
+      // url: `https://rpc.ankr.com/bsc_testnet_chapel/${process.env.API_KEY}`,
+      url: `https://bsc-testnet.public.blastapi.io`,
       accounts,
       chainId: 97,
+    },
+    arb: {
+      url: `https://arbitrum.blockpi.network/v1/rpc/public`,
+      accounts:{
+        mnemonic: "test test test test test test test test test test test junk"
+      }
+    },
+    arb2: {
+      url: `https://arbitrum-one.public.blastapi.io`,
+      accounts:{
+        mnemonic: "test test test test test test test test test test test junk"
+      }
     }
   }
 };
@@ -28,7 +41,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre)=>{
 })
 
 task("my_accounts", "Prints the list of accounts", async (taskArgs, hre)=>{
-  const account = hre.ethers.Wallet.fromMnemonic(process.env.MNEMONIC as string).connect(new hre.ethers.providers.JsonRpcProvider(hre.config.networks["bsc_testnet"].url));
+  const account = hre.ethers.Wallet.fromMnemonic(process.env.MNEMONIC as string).connect(new hre.ethers.providers.JsonRpcProvider((<HttpNetworkConfig>hre.config.networks["bsc_testnet"]).url));
   console.log(account.address)
   console.log("balance:", hre.ethers.utils.formatEther(await account.getBalance()))
   console.log("chainId:", await account.getChainId())
